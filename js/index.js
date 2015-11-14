0053446890b320bd5eb9d47e4ac632e0ec1340b5
AlexandrPetrikov <mojref@gmail.com>
2015-11-14 23:28:57 +0200

    add fixed menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+
 $("a[href^='#']").click(function(){
-	$("html,body").animate({scrollTop: $("a[name='" + this.hash.slice(1) + "']").offset().top},300);
+	$("html,body").animate({scrollTop: $("a[name='" + this.hash.slice(1) + "']").offset().top - 100},300);
 	return false;
+});
+/* Elastik menu*/
+var menuTop = $("#top-nav");
+var menuTopPosition = $("#top-nav").offset().top;
+var currentMenuTopPos = $("#top-nav").scrollTop();
+$(window).scroll(function(){
+	if($(window).scrollTop()>=menuTopPosition){
+		menuTop.addClass("top-nav-retrofit")
+		.css("margin-top",-menuTopPosition);
+	}else{
+		menuTop.removeClass("top-nav-retrofit")
+				.css("margin-top", 0);
+	};	
 });
 
 });
