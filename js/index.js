0977713e18e63f778aacb83527209144ddfa78a0
AlexandrPetrikov <mojref@gmail.com>
2015-11-21 15:58:38 +0200

    add :hover and parallax

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $("a[href^='#']").click(function(){
 				  .offset().top - 100},300);
 	return false;
 });
+
 /* Elastik menu*/
 var menuTop = $("#top-nav");
 var menuTopPosition = $("#top-nav").offset().top;
@@ -17,7 +18,19 @@ $(window).scroll(function(){
 	}else{
 		menuTop.removeClass("top-nav-retrofit")
 				.css("margin-top", 0);
-	};	
+	};
+		
+});
+
+/*paralax effect*/
+$("header[data-type='background']").each(function(){
+	var bgobj = $(this);
+	$(window).scroll(function(){
+		var yPos = -($(window).scrollTop()/bgobj.data("speed"))+"px";
+		console.log(yPos);
+		bgobj.css({backgroundPositionY:yPos});
+	});
+	
 });
 
 });
